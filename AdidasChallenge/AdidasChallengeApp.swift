69f3a3fa84a8042d33211da411fe56bc02f847b1
Pedro Seruca <pedroseruca@gmail.com>
2021-08-09 19:35:06 +0100

    Create SwiftUI project

---
+++ AdidasChallenge/AdidasChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdidasChallengeApp.swift
+//  AdidasChallenge
+//
+//  Created by Pedro Seruca on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdidasChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
