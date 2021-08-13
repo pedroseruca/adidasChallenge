16697ed17fc269a3b496cc7b81724c3ea3fe338b
Pedro Seruca <pedroseruca@gmail.com>
2021-08-13 15:32:42 +0100

    Extracted ProductReviewsContent to a new file

--- AdidasChallenge/Components/ProductReviews/ProductReviewsContent/ProductReviewsContent.swift
+++ AdidasChallenge/Components/ProductReviews/ProductReviewsContent/ProductReviewsContent.swift
@@ -1,13 +1,13 @@
 //
-//  ProductReviewsView.swift
+//  ProductReviewsContent.swift
 //  AdidasChallenge
 //
-//  Created by Pedro Seruca on 12/08/2021.
+//  Created by Pedro Seruca on 13/08/2021.
 //
 
 import SwiftUI
 
-struct ProductReviewsView: View {
+struct ProductReviewsContent: View {
     private let viewModel: ProductReviewsViewModelProtocol
 
     init(viewModel: ProductReviewsViewModelProtocol) {
@@ -15,35 +15,35 @@ struct ProductReviewsView: View {
     }
 
     var body: some View {
-        VStack(alignment: .leading) {
-            ProductReviewsHeader(viewModel: viewModel)
-                .padding(.leading, 15)
-                .padding(.trailing, 15)
-            ProductReviewsContent(viewModel: viewModel)
+        if viewModel.ratingsCountValue == 0 {
+            Text(viewModel.noReviewsMessage)
+                .padding(.all, 15)
+        } else {
+            List(viewModel.indices) { index in
+                let viewModel = viewModel.productReviewCellViewModel(for: index)
+                ProductReviewCell(viewModel: viewModel)
+            }
+            // SwiftUI weird separators not aligned
+            .padding(.leading, -16)
         }
     }
 }
 
-struct ProductReviewsView_Previews: PreviewProvider {
+struct ProductReviewsContent_Previews: PreviewProvider {
     private static let viewModel = ProductReviewsViewModel(reviews: [
         makeReview(rating: 0, text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"),
         makeReview(rating: 2, text: "dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic "),
-        makeReview(rating: 7, text: "typesetting, remaining essentially unchanged."),
-        makeReview(rating: 10, text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing "),
-        makeReview(rating: 0, text: "test"),
-        makeReview(rating: 2, text: "test 2"),
-        makeReview(rating: 7, text: "test 3"),
-        makeReview(rating: 10, text: "test 4"),
+        makeReview(rating: 7, text: "typesetting, remaining essentially unchanged.")
     ])
     static func makeReview(rating: Int, text: String) -> ProductReview {
         ProductReview(productId: "HI333", locale: "en-us", rating: rating, text: text)
     }
 
     static var previews: some View {
-        ProductReviewsView(viewModel: viewModel)
+        ProductReviewsContent(viewModel: viewModel)
             .previewLayout(.sizeThatFits)
 
-        ProductReviewsView(viewModel: ProductReviewsViewModel(reviews: []))
+        ProductReviewsContent(viewModel: ProductReviewsViewModel(reviews: []))
             .previewLayout(.sizeThatFits)
     }
 }
