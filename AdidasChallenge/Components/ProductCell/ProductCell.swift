1e483fc2b5eaa167d24c1bb5bb8b0570c7e31d7d
Pedro Seruca <pedroseruca@gmail.com>
2021-08-15 13:26:51 +0100

    Fixed some UI defects on ProductCell

--- AdidasChallenge/Components/ProductCell/ProductCell.swift
+++ AdidasChallenge/Components/ProductCell/ProductCell.swift
@@ -23,8 +23,11 @@ struct ProductCell: View {
         HStack {
             AsyncImage(viewModel: viewModel.imageViewModel) {
                 ProgressView()
+                    .padding(.trailing, 10)
+                    .padding(.bottom, 10)
                     .frame(width: imageSize, height: imageSize)
                     .scaleEffect(1.5)
+                    
             } image: { image in
                 Image(uiImage: image)
                     .resizable()
@@ -39,7 +42,7 @@ struct ProductCell: View {
 
             ProductCellInfoView(viewModel: viewModel)
             Spacer()
-        }
+        }.contentShape(Rectangle())
     }
 }
 
@@ -49,6 +52,8 @@ struct ProductCell_Previews: PreviewProvider {
             ProductCell(viewModel: viewModel1)
                 .previewLayout(.sizeThatFits)
             ProductCell(viewModel: viewModel2)
+                .previewLayout(.sizeThatFits)
+            ProductCell(viewModel: viewModel3)
                 .previewLayout(.sizeThatFits)
         }
     }
@@ -77,10 +82,37 @@ private extension ProductCell_Previews {
         description: "Description Description Description Description",
         currency: "$",
         price: 160,
-        imgUrl: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy/da278c9c5e244068b32cac4d0125fedd_9366/FY2002_00_plp_standard.jpg"
+        imgUrl: ""
     )
 
     static let viewModel2 = makeViewModel(with: product2)
+}
+
+// MARK: Preview #3
+
+private extension ProductCell_Previews {
+    class MockImageLoader: ImageLoaderProtocol {
+        var statePublisher: Published<ImageLoaderState>.Publisher { $state }
+
+        @Published private var state: ImageLoaderState = .loading
+
+        func load(with imageWidth: Int?) {
+        }
+    }
+    private static let product3 = Product(
+        id: "HI333",
+        name: "Cassina pt",
+        description: "Description Description Description Description",
+        currency: "$",
+        price: 160,
+        imgUrl: ""
+    )
+
+    static let viewModel3: ProductCellViewModelProtocol = {
+        let imageLoader = MockImageLoader()
+        return ProductCellViewModel(product: product3,
+                                    imageLoader: imageLoader)
+    }()
 }
 
 private extension ProductCell_Previews {
