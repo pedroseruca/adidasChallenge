56c9a79fea6abc9869b3172775980acad5358e39
Pedro Seruca <pedroseruca@gmail.com>
2021-08-16 11:06:16 +0100

    Added missing preview on AddReviewView

--- AdidasChallenge/Scenes/AddReviewView/AddReviewView.swift
+++ AdidasChallenge/Scenes/AddReviewView/AddReviewView.swift
@@ -5,22 +5,23 @@
 //  Created by Pedro Seruca on 16/08/2021.
 //
 
+import Combine
 import SwiftUI
 
 struct AddReviewView: View {
     @Environment(\.presentationMode) var presentation
-    
+
     private var viewModel: AddReviewViewModel
-    
+
     init(viewModel: AddReviewViewModel) {
         self.viewModel = viewModel
     }
-    
+
     @State private var text: String = ""
     @State private var rating: Float = 5
-    
-    private var ratingRound: Int { Int(rating)}
-    
+
+    private var ratingRound: Int { Int(rating) }
+
     var body: some View {
         VStack(alignment: .leading) {
             Text(viewModel.titleLabel)
@@ -29,7 +30,7 @@ struct AddReviewView: View {
             HStack {
                 Text("\(ratingRound)")
                     .font(.title2)
-                Slider(value: $rating, in: 0...10, step: 1)
+                Slider(value: $rating, in: 0 ... 10, step: 1)
             }
             Text(viewModel.shareOpinionLabel)
                 .font(.title3)
@@ -45,7 +46,7 @@ struct AddReviewView: View {
             }
             .padding(0)
             .buttonStyle(PrimaryButtonStyle(color: .black))
-                
+
             Spacer()
         }
         .padding()
@@ -54,13 +55,37 @@ struct AddReviewView: View {
         .onReceive(viewModel.onSuccessSubmitPublisher) { _ in
             self.presentation.wrappedValue.dismiss()
         }
-        
     }
 }
 
-//struct AddReviewView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AddReviewView()
-//            .previewLayout(.sizeThatFits)
-//    }
-//}
+struct AddReviewView_Previews: PreviewProvider {
+    struct MockAdidasAPI: AdidasAPIReviewsProtocol {
+        let reviews: ProductReviews
+        func getReview(for productId: String) -> AnyPublisher<ProductReviews, Error> {
+            Future { $0(.success(reviews)) }
+                .eraseToAnyPublisher()
+        }
+
+        func postReview(for productId: String, review: ProductReview) -> AnyPublisher<Void, Error> {
+            Future { $0(.success(())) }
+                .eraseToAnyPublisher()
+        }
+    }
+
+    private static let product = Product(
+        id: "HI333",
+        name: "product",
+        description: "description",
+        currency: "$",
+        price: 4,
+        imgUrl: "https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/c7099422ccc14e44b406abec00ba6c96_9366/NMD_R1_V2_Shoes_Black_FY6862_01_standard.jpg"
+    )
+    private static let viewModel = AddReviewViewModel(
+        product: product,
+        adidasAPI: MockAdidasAPI(reviews: []),
+        onReviewSubmitted: {})
+    static var previews: some View {
+        AddReviewView(viewModel: viewModel)
+            .previewLayout(.sizeThatFits)
+    }
+}
